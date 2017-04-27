0386c28175954a279282713562c708184f201155
Alanna Zhou <alanna.zhou@gmail.com>
2017-04-27 11:36:42 -0700

    Add boggle code

---
+++ Boggle/src/BoardFirstAutoPlayer.java
@@ -0,0 +1,27 @@
+
+/**
+ * 
+ * TODO Write a one-sentence summary of your class here. TODO Follow it with
+ * additional details about its purpose, what abstraction it represents, and how
+ * to use it.
+ *
+ * @author Anvitha, Alanna, Jessica
+ * @version Apr 27, 2017
+ * @author Period: 5
+ * @author Assignment: Boggle
+ *
+ * @author Sources: TODO
+ */
+public class BoardFirstAutoPlayer extends AbstractAutoPlayer
+{
+
+    @Override
+    public void findAllValidWords(
+        BoggleBoard board,
+        ILexicon lex,
+        int minLength )
+    {
+        // TODO: you write this code
+    }
+
+}
