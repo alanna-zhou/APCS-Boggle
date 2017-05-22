2c078d5a2039031fed5e450ddbf2500286310475
Anvitha Aluri <aaluri@gmail.com>
2017-05-22 11:58:34 -0700

    Update boardfirstautoplayer

--- Boggle/src/BoardFirstAutoPlayer.java
+++ Boggle/src/BoardFirstAutoPlayer.java
@@ -14,14 +14,18 @@ import java.util.List;
  *
  * @author Sources: none
  */
-public class BoardFirstAutoPlayer extends AbstractAutoPlayer {
+public class BoardFirstAutoPlayer extends AbstractAutoPlayer 
+{
 
-	public void findAllValidWords(BoggleBoard board, ILexicon lex, int minLength) {
+	public void findAllValidWords(BoggleBoard board, ILexicon lex, int minLength) 
+	{
 		myWords.clear();
 		List<BoardCell> listOfCells = new ArrayList<BoardCell>();
 		StringBuilder s = new StringBuilder();
-		for (int i = 0; i < board.size(); i++) {
-			for (int j = 0; j < board.size(); j++) {
+		for (int i = 0; i < board.size(); i++) 
+		{
+			for (int j = 0; j < board.size(); j++) 
+			{
 				// go through helper for every cell on the board
 				helper(board, lex, minLength, i, j, listOfCells, s);
 			}
@@ -29,16 +33,19 @@ public class BoardFirstAutoPlayer extends AbstractAutoPlayer {
 	}
 
 	private void helper(BoggleBoard board, ILexicon lex, int minLength, int r, int c, List<BoardCell> listOfCells,
-			StringBuilder s) {
+			StringBuilder s) 
+	{
 
 		// don't continue if r/c out of bounds
-		if (r < 0 || r >= board.size() || c < 0 || c >= board.size()) {
+		if (r < 0 || r >= board.size() || c < 0 || c >= board.size()) 
+		{
 			return;
 		}
 
 		BoardCell cell = new BoardCell(r, c);
 		// don't continue if computer returns to the same cell
-		if (listOfCells.contains(cell)) {
+		if (listOfCells.contains(cell)) 
+		{
 			return;
 		}
 
@@ -50,11 +57,12 @@ public class BoardFirstAutoPlayer extends AbstractAutoPlayer {
 		s.append(letter);
 
 		// if the string is either a word or a prefix, continue the search
-		if (lex.wordStatus(s).equals(LexStatus.PREFIX) || lex.wordStatus(s).equals(LexStatus.WORD)) {
-
+		if (lex.wordStatus(s).equals(LexStatus.PREFIX) || lex.wordStatus(s).equals(LexStatus.WORD)) 
+		{
 			// if the word/prefix is a correct word, add it to the list of
 			// correct words
-			if (lex.wordStatus(s).equals(LexStatus.WORD) && (s.length() >= minLength)) {
+			if (lex.wordStatus(s).equals(LexStatus.WORD) && (s.length() >= minLength)) 
+			{
 				add(s.toString());
 			}
 
@@ -62,18 +70,19 @@ public class BoardFirstAutoPlayer extends AbstractAutoPlayer {
 			int[] cdelta = { -1, 0, 1, -1, 1, -1, 0, 1 };
 			// traverse through the surrounding cells to see if they form words
 			// with the prefix
-			for (int k = 0; k < rdelta.length; k++) {
+			for (int k = 0; k < rdelta.length; k++) 
+			{
 				helper(board, lex, minLength, r + rdelta[k], c + cdelta[k], listOfCells, s);
 			}
 		}
 
 		listOfCells.remove(cell);
-		// for qu
-		if (letter.length() > 1) {
+		// check for qu
+		if (letter.length() > 1) 
+		{
 			s.setLength(s.length() - 2);
-
-		} 
-		if (letter.length() == 1)
+		}
+		if (letter.length() == 1) 
 		{
 			s.deleteCharAt(s.length() - 1);
 		}
