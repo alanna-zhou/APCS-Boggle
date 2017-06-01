409e3eb0b525512de2b3c9be722af3f877362065
Anvitha Aluri <aaluri@gmail.com>
2017-05-31 22:56:08 -0700

    Add javadoc comments

--- Boggle/src/AbstractAutoPlayer.java
+++ Boggle/src/AbstractAutoPlayer.java
@@ -1,34 +1,29 @@
 import java.util.SortedSet;
 
-
 /**
  * Augments <code>AutoPlayer</code> by adding abstract method for autoplayer.
  * 
  * @author Owen Astrachan
  *
  */
-public abstract class AbstractAutoPlayer extends AbstractPlayer
-                implements
-                IAutoPlayer
-{
+public abstract class AbstractAutoPlayer extends AbstractPlayer implements IAutoPlayer {
 
-    /**
-     * Requires that an auto player be able to find all the possible words on a
-     * boggle board.
-     */
-    public abstract void findAllValidWords(
-        BoggleBoard board,
-        ILexicon lex,
-        int minLength );
+	/**
+	 * Requires that an auto player be able to find all the possible words on a
+	 * boggle board.
+	 * 
+	 * @param board
+	 * @param lex
+	 * @param minLength
+	 */
+	public abstract void findAllValidWords(BoggleBoard board, ILexicon lex, int minLength);
 
-
-    /**
-     * Return string "AutoPlayer".
-     * 
-     * @return this player's name
-     */
-    public String getName()
-    {
-        return "AutoPlayer";
-    }
+	/**
+	 * Return string "AutoPlayer".
+	 * 
+	 * @return this player's name
+	 */
+	public String getName() {
+		return "AutoPlayer";
+	}
 }
--- Boggle/src/BoardFirstAutoPlayer.java
+++ Boggle/src/BoardFirstAutoPlayer.java
@@ -14,63 +14,53 @@ import java.util.List;
  *
  * @author Sources: none
  */
-public class BoardFirstAutoPlayer extends AbstractAutoPlayer 
-{
+public class BoardFirstAutoPlayer extends AbstractAutoPlayer {
 
+	/**
+	 * Uses recursive backtracking search to find all words on a Boggle board by
+	 * calling helper() and going through each cell on the board
+	 * 
+	 * @param board
+	 * @param lex
+	 * @param minLength
+	 */
+	public void findAllValidWords(BoggleBoard board, ILexicon lex, int minLength) {
+		myWords.clear();
+		List<BoardCell> listOfCells = new ArrayList<BoardCell>();
+		StringBuilder s = new StringBuilder();
+		for (int i = 0; i < board.size(); i++) {
+			for (int j = 0; j < board.size(); j++) {
+				// go through helper for every cell on the board
+				helper(board, lex, minLength, i, j, listOfCells, s);
+			}
+		}
+	}
 
-    /**
-     *
-     */
-    public void findAllValidWords(
-        BoggleBoard board,
-        ILexicon lex,
-        int minLength )
-    {
-        myWords.clear();
-        List<BoardCell> listOfCells = new ArrayList<BoardCell>();
-        StringBuilder s = new StringBuilder();
-        for ( int i = 0; i < board.size(); i++ )
-        {
-            for ( int j = 0; j < board.size(); j++ )
-            {
-                // go through helper for every cell on the board
-                helper( board, lex, minLength, i, j, listOfCells, s );
-            }
-        }
-    }
-
-
-    /**
-     * TODO Write your method description here.
-     * 
-     * @param board
-     * @param lex
-     * @param minLength
-     * @param r
-     * @param c
-     * @param listOfCells
-     * @param s
-     */
-    private void helper(
-        BoggleBoard board,
-        ILexicon lex,
-        int minLength,
-        int r,
-        int c,
-        List<BoardCell> listOfCells,
-        StringBuilder s )
-    {
+	/**
+	 * findAllValidWords's helper method. Finds all the words starting at a
+	 * specific given row and column and a given string builder. It searches all
+	 * the cells around it for viable words and adds them all to the myWords
+	 * treeSet defined in the AbstractPlayer class.
+	 * 
+	 * @param board
+	 * @param lex
+	 * @param minLength
+	 * @param r
+	 * @param c
+	 * @param listOfCells
+	 * @param s
+	 */
+	private void helper(BoggleBoard board, ILexicon lex, int minLength, int r, int c, List<BoardCell> listOfCells,
+			StringBuilder s) {
 
 		// don't continue if r/c out of bounds
-		if (r < 0 || r >= board.size() || c < 0 || c >= board.size()) 
-		{
+		if (r < 0 || r >= board.size() || c < 0 || c >= board.size()) {
 			return;
 		}
 
 		BoardCell cell = new BoardCell(r, c);
 		// don't continue if computer returns to the same cell
-		if (listOfCells.contains(cell)) 
-		{
+		if (listOfCells.contains(cell)) {
 			return;
 		}
 
@@ -82,12 +72,10 @@ public class BoardFirstAutoPlayer extends AbstractAutoPlayer
 		s.append(letter);
 
 		// if the string is either a word or a prefix, continue the search
-		if (lex.wordStatus(s).equals(LexStatus.PREFIX) || lex.wordStatus(s).equals(LexStatus.WORD)) 
-		{
+		if (lex.wordStatus(s).equals(LexStatus.PREFIX) || lex.wordStatus(s).equals(LexStatus.WORD)) {
 			// if the word/prefix is a correct word, add it to the list of
 			// correct words
-			if (lex.wordStatus(s).equals(LexStatus.WORD) && (s.length() >= minLength)) 
-			{
+			if (lex.wordStatus(s).equals(LexStatus.WORD) && (s.length() >= minLength)) {
 				add(s.toString());
 			}
 
@@ -95,20 +83,17 @@ public class BoardFirstAutoPlayer extends AbstractAutoPlayer
 			int[] cdelta = { -1, 0, 1, -1, 1, -1, 0, 1 };
 			// traverse through the surrounding cells to see if they form words
 			// with the prefix
-			for (int k = 0; k < rdelta.length; k++) 
-			{
+			for (int k = 0; k < rdelta.length; k++) {
 				helper(board, lex, minLength, r + rdelta[k], c + cdelta[k], listOfCells, s);
 			}
 		}
 
 		listOfCells.remove(cell);
 		// check for qu
-		if (letter.length() > 1) 
-		{
+		if (letter.length() > 1) {
 			s.setLength(s.length() - 2);
 		}
-		if (letter.length() == 1) 
-		{
+		if (letter.length() == 1) {
 			s.deleteCharAt(s.length() - 1);
 		}
 	}
