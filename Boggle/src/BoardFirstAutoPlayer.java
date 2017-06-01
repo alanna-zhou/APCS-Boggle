67a9f6f16f7d760e674c4786fbc3ac6be24a0ad6
Anvitha Aluri <aaluri@gmail.com>
2017-05-31 17:54:52 -0700

    Update JUnitTestAutoPlayer

--- Boggle/src/BoardFirstAutoPlayer.java
+++ Boggle/src/BoardFirstAutoPlayer.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
 /**
  * Finds all words quickly, facilitates a game of Boggle by finding every
  * possible word on a given Boggle board. Uses lexicons from the previous
@@ -15,95 +14,77 @@ import java.util.List;
  *
  * @author Sources: none
  */
-public class BoardFirstAutoPlayer extends AbstractAutoPlayer
+public class BoardFirstAutoPlayer extends AbstractAutoPlayer 
 {
 
-    public void findAllValidWords(
-        BoggleBoard board,
-        ILexicon lex,
-        int minLength )
-    {
-        myWords.clear();
-        List<BoardCell> listOfCells = new ArrayList<BoardCell>();
-        StringBuilder s = new StringBuilder();
-        for ( int i = 0; i < board.size(); i++ )
-        {
-            for ( int j = 0; j < board.size(); j++ )
-            {
-                // go through helper for every cell on the board
-                helper( board, lex, minLength, i, j, listOfCells, s );
-            }
-        }
-    }
+	public void findAllValidWords(BoggleBoard board, ILexicon lex, int minLength) 
+	{
+		clear();
+		List<BoardCell> listOfCells = new ArrayList<BoardCell>();
+		StringBuilder s = new StringBuilder();
+		for (int i = 0; i < board.size(); i++) 
+		{
+			for (int j = 0; j < board.size(); j++) 
+			{
+				// go through helper for every cell on the board
+				helper(board, lex, minLength, i, j, listOfCells, s);
+			}
+		}
+	}
 
+	private void helper(BoggleBoard board, ILexicon lex, int minLength, int r, int c, List<BoardCell> listOfCells,
+			StringBuilder s) 
+	{
 
-    private void helper(
-        BoggleBoard board,
-        ILexicon lex,
-        int minLength,
-        int r,
-        int c,
-        List<BoardCell> listOfCells,
-        StringBuilder s )
-    {
+		// don't continue if r/c out of bounds
+		if (r < 0 || r >= board.size() || c < 0 || c >= board.size()) 
+		{
+			return;
+		}
 
-        // don't continue if r/c out of bounds
-        if ( r < 0 || r >= board.size() || c < 0 || c >= board.size() )
-        {
-            return;
-        }
+		BoardCell cell = new BoardCell(r, c);
+		// don't continue if computer returns to the same cell
+		if (listOfCells.contains(cell)) 
+		{
+			return;
+		}
 
-        BoardCell cell = new BoardCell( r, c );
-        // don't continue if computer returns to the same cell
-        if ( listOfCells.contains( cell ) )
-        {
-            return;
-        }
+		// a letter at (r,c)
+		String letter = board.getFace(r, c);
+		// add this cell to the list of cells already passed through
+		listOfCells.add(cell);
+		// add letter to the end of the string built so far
+		s.append(letter);
 
-        // a letter at (r,c)
-        String letter = board.getFace( r, c );
-        // add this cell to the list of cells already passed through
-        listOfCells.add( cell );
-        // add letter to the end of the string built so far
-        s.append( letter );
+		// if the string is either a word or a prefix, continue the search
+		if (lex.wordStatus(s).equals(LexStatus.PREFIX) || lex.wordStatus(s).equals(LexStatus.WORD)) 
+		{
+			// if the word/prefix is a correct word, add it to the list of
+			// correct words
+			if (lex.wordStatus(s).equals(LexStatus.WORD) && (s.length() >= minLength)) 
+			{
+				add(s.toString());
+			}
 
-        // if the string is either a word or a prefix, continue the search
-        if ( lex.wordStatus( s ).equals( LexStatus.PREFIX )
-            || lex.wordStatus( s ).equals( LexStatus.WORD ) )
-        {
-            // if the word/prefix is a correct word, add it to the list of
-            // correct words
-            if ( lex.wordStatus( s ).equals( LexStatus.WORD )
-                && ( s.length() >= minLength ) )
-            {
-                add( s.toString() );
-            }
+			int[] rdelta = { -1, -1, -1, 0, 0, 1, 1, 1 };
+			int[] cdelta = { -1, 0, 1, -1, 1, -1, 0, 1 };
+			// traverse through the surrounding cells to see if they form words
+			// with the prefix
+			for (int k = 0; k < rdelta.length; k++) 
+			{
+				helper(board, lex, minLength, r + rdelta[k], c + cdelta[k], listOfCells, s);
+			}
+		}
 
-            int[] rdelta = { -1, -1, -1, 0, 0, 1, 1, 1 };
-            int[] cdelta = { -1, 0, 1, -1, 1, -1, 0, 1 };
-            // traverse through the surrounding cells to see if they form words
-            // with the prefix
-            for ( int k = 0; k < rdelta.length; k++ )
-            {
-                helper( board,
-                    lex,
-                    minLength,
-                    r + rdelta[k],
-                    c + cdelta[k],
-                    listOfCells,
-                    s );
-            }
-        }
-
-        listOfCells.remove( cell );
-        // check for qu
-        if ( letter.length() > 1 )
-        {
-            s.setLength( s.length() - 2 );
-        }
-        if ( letter.length() == 1 )
-        {
-            s.deleteCharAt( s.length() - 1 );
-        }
-    }
+		listOfCells.remove(cell);
+		// check for qu
+		if (letter.length() > 1) 
+		{
+			s.setLength(s.length() - 2);
+		}
+		if (letter.length() == 1) 
+		{
+			s.deleteCharAt(s.length() - 1);
+		}
+	}
 }
--- Boggle/src/JUnitTestAutoPlayer.java
+++ Boggle/src/JUnitTestAutoPlayer.java
@@ -1,11 +1,139 @@
-
 import static org.junit.Assert.*;
-
 import org.junit.*;
 import java.util.*;
 
+/**
+ * JUnit Tests for the BoardFirstAutoPlayer. Tests if it correctly finds words
+ * on the board and correctly returns the list of board cells that are
+ * associated with the letters of the word.
+ * 
+ */
+public class JUnitTestAutoPlayer {
 
-public class JUnitTestAutoPlayer
-{
+	private IBoardMaker myMaker;
+
+	private IAutoPlayer autoPlayer;
+
+	/**
+	 * Creates the board needed to find the words on.
+	 * 
+	 */
+	public class LocalBoardMaker implements IBoardMaker {
+
+		private String[] good = { "atru", "sean", "niot", "bden" };
+
+		public BoggleBoard makeBoard(int rows) {
+			String[] faces = new String[good.length * good.length];
+			int count = 0;
+			for (String s : good) {
+				for (int k = 0; k < s.length(); k++)
+					faces[count++] = "" + s.charAt(k);
+			}
+			return new BoggleBoard(faces);
+		}
+	}
+
+	/**
+	 * Initializes the board maker and finder constructors
+	 */
+	@Before
+	public void setUp() {
+		myMaker = new LocalBoardMaker();
+		autoPlayer = new BoardFirstAutoPlayer();
+	}
+
+	/**
+	 * To string method for the list of BoardCell objects in the list.
+	 * 
+	 * @param board
+	 *            boggle board
+	 * @param list
+	 *            list of BoardCell objects
+	 * @return String representation of the list
+	 */
+	private String getWord(BoggleBoard board, List<BoardCell> list) {
+		String word = "";
+		for (BoardCell cell : list) {
+			word += board.getFace(cell.row, cell.col);
+		}
+		return word;
+	}
+
+	/**
+	 * BoardFirstAutoPlayer is expected to find these words in the corner of the
+	 * board.
+	 */
+	@Test
+	public void testGoodCorners() {
+		ILexicon lex = new BinarySearchLexicon();
+		String[] cornerWords = { "ate", "noted", "net", "urn", "bind", "aside", "noise" };
+		BoggleBoard board = myMaker.makeBoard(3);
+		for (String s : cornerWords) {
+			autoPlayer.findAllValidWords(board, lex, board.size());
+			Iterator<String> iter = autoPlayer.iterator();
+			while (iter.hasNext()) {
+				String word = iter.next();
+				System.out.println(word);
+			}
+			// List<BoardCell> list = myFinder.cellsForWord( board, s );
+			// String word = getWord( board, list );
+			// assertEquals( "fail for " + s, s, word );
+		}
+	}
+
+	/**
+	 * GoodWordOnBoardFinder is expected to find these words that are not in the
+	 * corner of the board.
+	 */
+	@Test
+	public void testNonCorners() {
+		String[] words = { "reinstate", "resident", "insert", "trains", "treated" };
+		BoggleBoard board = myMaker.makeBoard(4);
+
+		for (String s : words) {
+			List<BoardCell> list = myFinder.cellsForWord(board, s);
+			String word = getWord(board, list);
+			assertEquals("fail for " + s, s, word);
+		}
+	}
+
+	/**
+	 * GoodWordOnBoardFinder is expected to return an empty list because these
+	 * should not be words found.
+	 */
+	@Test
+	public void testBadCorners() {
+		String[] cornerWords = { "notary", "urine", "need", "diners", "astride", "nosier" };
+		BoggleBoard board = myMaker.makeBoard(4);
+		for (String s : cornerWords) {
+			List<BoardCell> list = myFinder.cellsForWord(board, s);
+			String word = getWord(board, list);
+			assertNotSame("fail for " + s, s, word);
+		}
+	}
+
+	/**
+	 * GoodWordOnBoardFinder is expected to return false for duplicate cells.
+	 */
+	@Test
+	public void testDuplicates() {
+		String[] faces = { "t", "o", "h", "o", "e", "t", "p", "n", "o", "a", "e", "e", "p", "t", "y", "s" };
+		BoggleBoard board = new BoggleBoard(faces);
+
+		List<BoardCell> list = myFinder.cellsForWord(board, "tet");
+		List<BoardCell> correctList = new ArrayList<BoardCell>();
+		correctList.add(new BoardCell(0, 0));
+		correctList.add(new BoardCell(0, 1));
+		correctList.add(new BoardCell(1, 1));
+		assertNotSame("fail for tet", correctList, list);
+
+		List<BoardCell> list1 = myFinder.cellsForWord(board, "ene");
+		List<BoardCell> correctList1 = new ArrayList<BoardCell>();
+		correctList.add(new BoardCell(2, 2));
+		correctList.add(new BoardCell(1, 3));
+		correctList.add(new BoardCell(2, 3));
+		assertNotSame("fail for ene", correctList1, list1);
+
+	}
 
 }
